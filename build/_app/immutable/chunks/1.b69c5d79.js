c2c30e9cdbf6264fcca23b33252ab8fc747303bf
Roland M <rolandmackintosh1@gmail.com>
2025-02-03 22:35:34 -0800

    non-understood changes from ages ago

--- build/_app/immutable/chunks/1.b69c5d79.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.5deabd18.js";export{t as component};
--- build/_app/immutable/chunks/2.cc080ddf.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/_page.svelte.00384491.js";export{t as component};
